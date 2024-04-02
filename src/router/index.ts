bd5bb9b5f46ebfe117a827b13db3ea6684f729a5
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2024-04-02 22:12:06 +0100

    tidy up

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,7 +8,6 @@
 import { createRouter, createWebHistory } from "vue-router/auto";
 import { setupLayouts } from "virtual:generated-layouts";
 
-console.log(`base url is ${import.meta.env.BASE_URL}`);
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     extendRoutes: setupLayouts
