81c2e2677bb2700958bc876fe95f89bbbf434606
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2024-04-15 16:24:23 +0100

    add alias for indicator

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,10 +8,13 @@
 import { createRouter, createWebHistory } from "vue-router/auto";
 import { setupLayouts } from "virtual:generated-layouts";
 
+const APP = "dengue";
+
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHistory(`${import.meta.env.BASE_URL}/${APP}`),
     extendRoutes: setupLayouts
 });
-router.addRoute({ path: '/', redirect: '/dengue' });
+// TODO: on browse to /${APP}, if no :indictaor, redirect to first indicator
+router.addRoute({ path: `/${APP}/:indicator`, alias: `/${APP}`, props: true });
 
 export default router;
