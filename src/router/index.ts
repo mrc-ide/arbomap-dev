2eb399d6fa2dfb54dcac91259f0b70f89d92302a
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2024-04-22 10:16:11 +0100

    remove commented out bits

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,22 +7,10 @@ import {baseUrl, PATHOGEN} from "./utils";
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
-        //{ path: "/", component: index, props: true },
-        //{ path: "/:pathogen", component: index, props: true },
-        //{ path: "/:pathogen/:version", component: index, props: true },
-        //{ path: "/:pathogen/:version/:indicator", component: index, props: true },
         { path: "/:pathogen?/:version?/:indicator?/:country?", component: index, props: true },
         { path: `/about`, component: about },
-        //{ path: "/:indicator", component: index, props: true },
-        //{ path: "/:indicator/:country", component: index, props: true },
         { path: "/:pathMatch(.*)", component: notFound}
     ]
 });
-
-// This route is required for non-root base urls where (I think) vite defaults to serving the base url, which
-// can clash with the configuration in vue router
-//if (baseUrl !== "/") {
-//    router.addRoute({ path: baseUrl, redirect: "/"  });
-//}
 
 export default router;
