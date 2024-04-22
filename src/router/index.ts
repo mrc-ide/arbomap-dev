769581f396af2a3745a735448c1ea866ff6f01ac
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2024-04-22 14:30:49 +0100

    remove unused

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from "vue-router/auto";
 import index from "../pages/index.vue";
 import about from "../pages/about.vue";
 import notFound from "../pages/notFound.vue";
-import {baseUrl, PATHOGEN} from "./utils";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
