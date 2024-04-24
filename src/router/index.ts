256edbeaf7c6cebf9f2fe861f189a540c5d99154
David Mears <clarinet.david@gmail.com>
2024-04-24 10:59:53 +0100

    Hard-code pathogen in routes for static pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,14 +4,15 @@ import about from "../pages/about.vue";
 import accessibility from "../pages/accessibility.vue";
 import privacy from "../pages/privacy.vue";
 import notFound from "../pages/notFound.vue";
+import { PATHOGEN } from "./utils";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         { path: "/:pathogen?/:version?/:indicator?/:country?", component: index, props: true },
-        { path: "/:pathogen?/about", component: about },
-        { path: "/:pathogen?/accessibility", component: accessibility },
-        { path: "/:pathogen?/privacy", component: privacy },
+        { path: `/${PATHOGEN}/about`, component: about },
+        { path: `/${PATHOGEN}/accessibility`, component: accessibility },
+        { path: `/${PATHOGEN}/privacy`, component: privacy },
         { path: "/:pathMatch(.*)", component: notFound}
     ]
 });
