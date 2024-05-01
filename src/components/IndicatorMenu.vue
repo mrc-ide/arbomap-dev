5d9e7ffa440a6e33414381af7bf78eea41a2c633
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2024-05-01 14:45:07 +0100

    add submenu for stratified indicators

--- src/components/IndicatorMenu.vue
+++ src/components/IndicatorMenu.vue
@@ -4,15 +4,32 @@
             {{ appConfig.indicators[selectedIndicator].humanReadableName }}
             <v-menu activator="parent">
                 <v-list>
-                    <v-list-item
-                        v-for="id in indicatorGroupIds"
-                        :key="id"
-                        :value="id"
-                        :title="appConfig.indicators[id].humanReadableName"
-                        :subtitle="appConfig.indicators[id].description"
-                        :active="id === selectedIndicator"
-                    >
-                    </v-list-item>
+                    <template v-for="(id, index) in indicatorGroupIds" :key="id">
+                        <template v-if="appConfig.indicatorGroups[index].subIndicators">
+                            <v-btn class="float-right">
+                                ...
+                                <v-menu activator="parent" offset-x>
+                                    <v-list>
+                                        <v-list-item v-for="subId in appConfig.indicatorGroups[index].subIndicators"
+                                                     :key="subId"
+                                                     :value="subId"
+                                                     :title="appConfig.indicators[subId].humanReadableName"
+                                                     :subtitle="appConfig.indicators[subId].description"
+                                                     :active="subId === selectedIndicator"
+                                        >
+                                        </v-list-item>
+                                    </v-list>
+                                </v-menu>
+                            </v-btn>
+                        </template>
+                        <v-list-item
+                            :value="id"
+                            :title="appConfig.indicators[id].humanReadableName"
+                            :subtitle="appConfig.indicators[id].description"
+                            :active="id === selectedIndicator"
+                        >
+                        </v-list-item>
+                    </template>
                 </v-list>
             </v-menu>
         </v-btn>
