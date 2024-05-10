1c047f2344b5735e8a7136ae3d4669ce630d61cc
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2024-05-10 15:53:23 +0100

    new data, including updated geojson for india

--- scripts/process-data.R
+++ scripts/process-data.R
@@ -41,6 +41,11 @@ process_country <- function(x, country_codes, level) {
 process <- function(path, dest, level) {
   dat <- readxl::read_excel(path)
   dir.create(dest, FALSE, TRUE)
+  countries <- unique(dat$ID_0)
+  # we deliberately exclude these countries which have values derived from
+  # neighbouring countries
+  countries <- countries[!(countries %in% c("ASM", "CYM", "DMA", "KNA", "MNP", "MSR", "NRU", "PLW", "TCA", "TUV", "VGB", "WLF"))]
+
   if (level == 2) {
     for (iso in unique(dat$ID_0)) {
       json <- jsonlite::toJSON(
@@ -48,14 +53,14 @@ process <- function(path, dest, level) {
       writeLines(json, file.path(dest, paste0(iso, ".json")))
     }
   }
-  
+
   if (level == 1) {
     # output a single file, and also output array of countries
-    countries <- c()
+    #countries <- c()
     data_by_country <- list()
     for (iso in unique(dat$ID_0)) {
       data_by_country[[iso]] = process_country(dat[dat$ID_0 == iso, ], country_codes, level)
-      countries <- append(countries, iso)
+      #countries <- append(countries, iso)
     }
     json <- jsonlite::toJSON(data_by_country, auto_unbox = TRUE)
     writeLines(json, file.path(dest, paste0("global_adm1.json")))
@@ -63,12 +68,13 @@ process <- function(path, dest, level) {
   }
 }
 
-root <- here::here()
-process(file.path(root, "data/raw/Adm1_Estimates_v3_gadm41.xlsx"),
+#root <- here::here()
+root <- "/home/emma/dev/arbomap"
+process(file.path(root, "data/raw/Adm1_Estimates_v3_gadm41_filled_gaps.xlsx"),
         file.path(root, "data/processed/admin1"),
         1)
 process(file.path(root, "data/raw/Adm2_Estimates_v3_gadm41_filled_gaps.xlsx"),
         file.path(root, "data/processed/admin2"),
-        2)
+       2)
 
 
