4679550a288acc2ab8d55a7f4e672087e1bcb7a8
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2024-05-28 14:04:59 +0100

    some tidying

--- src/composables/useExcelDownload.ts
+++ src/composables/useExcelDownload.ts
@@ -1,4 +1,4 @@
-import {AppConfig, ColorType, FeatureIndicators, Geojson} from "../types/resourceTypes";
+import {AppConfig, ColorType, FeatureIndicators, Geojson, MapFeature} from "../types/resourceTypes";
 import {Dict} from "../types/utilTypes";
 import * as XLSX from "xlsx";
 import {geoJson} from "leaflet";
@@ -23,7 +23,7 @@ export const useExcelDownload = () => {
 
     const { getIndicatorColorType, getIndicatorValueColorCategory } = useIndicatorColors();
 
-    const writeTab = (workbook: WorkBook, level: number, indicatorValues: Dict<FeatureIndicators>, geojson: Dict<Geojson>, country?: string) => {
+    const writeTab = (workbook: WorkBook, level: number, indicatorValues: Dict<FeatureIndicators>, geojson: Dict<MapFeature[]>, country?: string) => {
         const sheetData = [];
 
         const { indicators, countries, geoJsonFeatureProperties } = appConfig.value;
@@ -33,13 +33,16 @@ export const useExcelDownload = () => {
         const indicatorIds = Object.keys(indicators);
 
         const categoryIndicators = Object.keys(indicators).filter((i) => getIndicatorColorType(i) === ColorType.Category);
-        console.log(`category indicators: ${JSON.stringify(categoryIndicators)}`)
+
+        // Include IDs and names for all region levels from 0 to the requested level
+        const levels = Array.from({length: level+1}, (_, v) => v);
+        const [, ...subCountryLevels] = levels;
 
         // We use the configured feature prop names as the column headers for ID and names
         const headers = [];
-        for (let i = 0; i <= level; i++) {
-            headers.push(geoJsonFeatureProperties[`idAdm${i}`], geoJsonFeatureProperties[`nameAdm${i}`]);
-        }
+        levels.forEach((level) => {
+            headers.push(geoJsonFeatureProperties[`idAdm${level}`], geoJsonFeatureProperties[`nameAdm${level}`]);
+        });
 
         indicatorIds.forEach((indicatorId) => {
             if (categoryIndicators.includes(indicatorId)) {
@@ -59,10 +62,7 @@ export const useExcelDownload = () => {
             const countryGeojson = geojson[countryId];
 
             // Iterate features in geojson, but only emit a row if we have values in the indicators
-            // Deal with inconsistency in admin1 vs admin2 geojson format
-            // TODO: fix this properly in types
-            const features = (countryGeojson.features || countryGeojson) as Feature[];
-            features.forEach((feature) => {
+            countryGeojson.forEach((feature) => {
                 const featureId = feature.properties[levelFeatureIdProp];
                 const featureValues = countryValues[featureId];
                 if (featureValues) {
@@ -71,13 +71,11 @@ export const useExcelDownload = () => {
                     //country level
                     row.push(countryId, countryName);
 
-                    //sub-country levels
-                    for (let i = 1; i <= level; i++) {
-                        // TODO: do something cleverer to save the level ids in an array
-                        const idProp = geoJsonFeatureProperties[`idAdm${i}`];
-                        const nameProp = geoJsonFeatureProperties[`nameAdm${i}`];
+                    subCountryLevels.forEach((level) => {
+                        const idProp = geoJsonFeatureProperties[`idAdm${level}`];
+                        const nameProp = geoJsonFeatureProperties[`nameAdm${level}`];
                         row.push(feature.properties[idProp], feature.properties[nameProp]);
-                    }
+                    });
 
                     indicatorIds.forEach((indicatorId) => {
                         const mean = featureValues[indicatorId].mean;
