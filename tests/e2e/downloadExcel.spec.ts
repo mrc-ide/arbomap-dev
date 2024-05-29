cb69ab5670be0ed2227ff7ee55ccbc9877d48076
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2024-05-29 10:47:54 +0100

    e2e tests

---
+++ tests/e2e/downloadExcel.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect, Page } from "@playwright/test";
+
+test.describe("Excel download button", () => {
+    const testCanDownloadFile = async (page: Page, expectedFilename: string) => {
+        const [download] = await Promise.all([
+            // Start waiting for the download
+            page.waitForEvent("download"),
+            // Perform the action that initiates download
+            page.click("#download-excel-btn")
+        ]);
+        // Wait for the download process to complete
+        await download.path();
+
+        expect(download.suggestedFilename()).toBe(expectedFilename);
+    };
+
+    test("can download global indicators", async ({page}) => {
+        await page.goto("/");
+        await testCanDownloadFile(page,"arbomap_GLOBAL.xlsx");
+    });
+
+    test("can download country indicators", async ({page}) => {
+        await page.goto("/dengue/may24/FOI/MEX");
+        await page.waitForURL(/dengue\/may24\/FOI\/MEX\/admin2/i);
+        await testCanDownloadFile(page, "arbomap_MEX.xlsx");
+    });
+});
