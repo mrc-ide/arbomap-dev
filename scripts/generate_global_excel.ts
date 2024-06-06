acd04062fe48c9b06c1750a7dd8b68b1dab95c9b
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2024-06-06 14:28:26 +0100

    add vite-node

---
+++ scripts/generate_global_excel.ts
@@ -0,0 +1,9 @@
+import { createServer } from 'vite';
+import { AdminLevel } from "../src/utils";
+import { useExcelDownload } from "../src/composables/useExcelDownload";
+
+const a=2;
+const b = AdminLevel.ONE;
+//const c = useExcelDownload();
+console.log("hello");
+console.log(b);
