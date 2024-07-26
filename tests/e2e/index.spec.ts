f82bb00c9c06baf68160f1ae2ca0462c42cbf0a5
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2024-07-26 09:18:06 +0100

    more test fix

--- tests/e2e/index.spec.ts
+++ tests/e2e/index.spec.ts
@@ -66,11 +66,11 @@ test.describe("Index page", () => {
         await firstRegion.hover();
         await expect(await page.innerText(".leaflet-tooltip-pane")).toContain("Bengo");
         await expect(await page.innerText(".leaflet-tooltip-pane")).toContain("Force of infection: 0.05");
-        await expect(await page.innerText(".leaflet-tooltip-pane")).toContain("Seroprevalence at age 9 years: 36.6%");
+        await expect(await page.innerText(".leaflet-tooltip-pane")).toContain("Seroprevalence at age 9 years: 40.9%");
         await expect(await page.innerText(".leaflet-tooltip-pane")).toContain(
-            "Seroprevalence classification at age 9 years: Under 40%"
+            "Seroprevalence classification at age 9 years: 40-60%"
         );
-        await expect(await page.innerText(".leaflet-tooltip-pane")).toContain("Hospital admissions: 139");
+        await expect(await page.innerText(".leaflet-tooltip-pane")).toContain("Hospital admissions: 156");
     });
 
     test("if no data, no data tooltip is shown", async ({ page }) => {
